5204f466908832a1f0fb36348b0194f980a06070
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-05-18 23:54:59 -0300

    :pencil: :recycle: refactoring, adding docs

--- index.js
+++ index.js
@@ -8,29 +8,41 @@ window.onload = function main() {
 
   inputSize.addEventListener("input", () => setMode(showMode, inputSize));
 
-  document.querySelector("#form").addEventListener("submit", e => {
+  document.querySelector("#form").addEventListener("submit", (e) => {
     e.preventDefault();
     initGrid();
   });
 };
 
 function setMode(showMode, { value }) {
-  showMode.innerHTML = Object.freeze(
-    {
-      3: () => "noob <span>&#x1F92A;</span>",
-      4: () => "amador <span>&#x1F923;</span>",
-      5: () => "PRO <span>&#x1F911;</span>"
-    }[value]()
-  );
+  showMode.innerHTML = {
+    3: () => "noob <span>&#x1F92A;</span>",
+    4: () => "amador <span>&#x1F923;</span>",
+    5: () => "PRO <span>&#x1F911;</span>",
+  }[value]();
 }
 
 function initGrid() {
   const gridContainer = document.querySelector("#grid-container");
 
   reset(gridContainer);
+
   startApp(gridContainer, gridSize(inputSize));
 }
 
+/**
+ * @param {HTMLElement} gridContainer
+ * @description removes this element's childNodes
+ * @example
+ * before:
+ * <div id="element">
+ *    <span></span>
+ *    <p></p>
+ * </div>
+ *
+ * after:
+ * <div id="element"></div>
+ */
 function reset(gridContainer) {
   while (gridContainer.hasChildNodes()) {
     gridContainer.removeChild(gridContainer.firstChild);
@@ -53,8 +65,8 @@ function startApp(gridContainer, size) {
   }
 
   const gridList = document.querySelectorAll(".grid-item");
-  gridList.forEach(item => {
-    item.addEventListener("pointerenter", e => cb(e));
+  gridList.forEach((item) => {
+    item.addEventListener("pointerenter", (e) => cb(e));
   });
 }
 
@@ -65,18 +77,16 @@ function cb({ target: { style } }) {
 
 function paint(style, actualColor) {
   style.backgroundColor = actualColor;
-  console.log(style);
 }
 
-function pickColor(style, mode) {
-  return Object.freeze(
-    {
-      black: () => "#000",
-      colored: () => colorfy(),
-      shine: () => new Shades(style).shine(),
-      fade: () => new Shades(style).fade()
-    }[mode]()
-  );
+function pickColor(style, color) {
+  const palette = {
+    black: () => "#000",
+    colored: () => colorfy(),
+    shine: () => new Shades(style).shine(),
+    fade: () => new Shades(style).fade(),
+  };
+  return palette[color]();
 }
 
 function colorfy() {
@@ -88,35 +98,62 @@ function randomize() {
 }
 
 class Shades {
-  constructor({ backgroundColor }) {
-    backgroundColor
-      ? (this.backgroundColor = backgroundColor)
-      : (this.backgroundColor = "rgb(255, 255, 255)");
+  /**
+   * @param { CSSStyleDeclaration } style
+   */
 
+  constructor(style) {
+    style.backgroundColor
+      ? (this.backgroundColor = style.backgroundColor)
+      : (this.backgroundColor = "rgb(255, 255, 255)");
     [this.red, this.green, this.blue] = validate(this.backgroundColor);
   }
 
   /**
-   * gets brighter until a 255 shows up in rgb
+   * @description gets brighter until a 255 shows up in rgb
+   * @returns {string}
    */
 
   shine() {
-    return [this.red, this.green, this.blue].filter(x => x >= 255)[0]
+    return [this.red, this.green, this.blue].filter((x) => x >= 255)[0]
       ? this.backgroundColor
       : `rgb(${this.red + 25}, ${this.green + 25}, ${this.blue + 25})`;
   }
 
   /**
-   * gets darker, until a 25 shows up in rgb
+   * @description gets darker, until a 25 shows up in rgb
+   * @returns {string}
    */
   fade() {
-    return [this.red, this.green, this.blue].filter(x => x <= 25)[0]
+    return [this.red, this.green, this.blue].filter((x) => x <= 25)[0]
       ? this.backgroundColor
       : `rgb(${this.red - 25}, ${this.green - 25}, ${this.blue - 25})`;
   }
 }
 
+/**
+ * @param {string} bgColor
+ * @description extract numbers from rgb
+ * @example
+ * let array = validate("rgb(1, 2, 3)")
+ * array === [1, 2, 3]
+ * @returns {number[]}
+ */
 function validate(bgColor) {
-  //return css rgb nums
-  return bgColor.match(/\d{1,3}/g).map(x => parseInt(x, 10));
+  return bgColor.match(/\d{1,3}/g).map((x) => parseInt(x, 10));
 }
+
+module.exports = {
+  setMode,
+  initGrid,
+  reset,
+  gridSize,
+  startApp,
+  cb,
+  paint,
+  pickColor,
+  colorfy,
+  randomize,
+  Shades,
+  validate,
+};
